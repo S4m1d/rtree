fcd43881fb0ddc4aab26902daf3092b7a88368a3
s4m1d <iams4m1d@gmail.com>
2024-08-18 23:33:25 +0500

    added simple current dir scan for files and directories

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,19 @@ fn main() -> std::io::Result<()> {
 
     for entry in cur_dir_entries {
         let entry = entry?;
+
         let file_name = entry.file_name();
+
+        let file_type = entry.file_type()?;
+
+        match file_type.is_dir() {
+            true => println!("{}/", file_name.to_string_lossy()),
+            false => match file_type.is_file() {
+                true => println!("{}", file_name.to_string_lossy()),
+                false => println!("{}", file_name.to_string_lossy()),
+            },
+        }
+
         println!("{}", file_name.to_string_lossy())
     }
     Ok(())
