d4ffcc76540e37e553f6139665661e7bcbfb7304
s4m1d <iams4m1d@gmail.com>
2024-08-19 00:23:16 +0500

    added flag toggle for hidden files include

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
-use std::{fs, path::Path};
+use std::{env, fs, path::Path};
 
 fn main() -> std::io::Result<()> {
+    // command line flags parsing
+    let args: Vec<String> = env::args().collect();
+
+    let mut is_include_hidden: bool = false;
+
+    for arg in args {
+        if arg == "-a" {
+            is_include_hidden = true;
+        }
+    }
+
+    // programm logic
     let cur_path = Path::new(".");
 
     let cur_dir_entries = fs::read_dir(cur_path)?;
@@ -9,6 +21,14 @@ fn main() -> std::io::Result<()> {
         let entry = entry?;
 
         let file_name = entry.file_name();
+
+        if file_name.len() > 0 && !is_include_hidden {
+            if let Some(first_char) = file_name.to_string_lossy().chars().next() {
+                if first_char == '.' {
+                    continue;
+                }
+            }
+        }
 
         let file_type = entry.file_type()?;
 
